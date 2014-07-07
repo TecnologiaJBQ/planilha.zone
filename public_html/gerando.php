a5eb8c4f43645d08c456e5694806de9d08af127d
Udlei Nattis <udlei@nati.biz>
2014-07-07 15:17:04 -0300

    Valor com virtual e adicionado maxvolume

--- public_html/gerando.php
+++ public_html/gerando.php
@@ -207,8 +207,10 @@ function calcular(v_count,faixa_ini,faixa_fim,cep_uso,peso_ini,peso_fim,peso_med
 
     peso_ini = parseInt(parseFloat(peso_ini) * 1000);
     peso_fim = parseInt(parseFloat(peso_fim) * 1000);
+    valor = data.Valor;
+    valor = valor.replace(".",",");
 
-    linha = faixa_ini.replace("-","") + ";" + faixa_fim.replace("-","") + ";" + peso_ini + ";" + peso_fim + ";" + data.Valor + ";0;0;0;" + data.PrazoEntrega + ";BRA;\n";
+    linha = faixa_ini.replace("-","") + ";" + faixa_fim.replace("-","") + ";" + peso_ini + ";" + peso_fim + ";" + valor + ";0;0;10000000;" + data.PrazoEntrega + ";BRA;\n";
     
     csv += linha;
 
