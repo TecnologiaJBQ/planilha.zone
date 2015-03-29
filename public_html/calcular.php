a7dc4cd57432ae8c992af1723247783b609ac85d
Udlei Nattis <udlei@protonmail.ch>
2015-03-29 14:17:54 +0000

    - Novas faixas para eSedex e validacao de erro.

--- public_html/calcular.php
+++ public_html/calcular.php
@@ -1,4 +1,7 @@
 <?php
+
+// SimpleXMLElement simplexml_load_file ( string $filename [, string $class_name = "SimpleXMLElement" [, int $options = 0 [, string $ns = "" [, bool $is_prefix = false ]]]] )
+
 function calculaFrete($cod_servico, $cep_origem, $cep_destino, $peso, $cod_adm, $senha, $altura='2', $largura='11', $comprimento='16', $valor_declarado='0.50')
 {
     #OFICINADANET###############################
@@ -10,15 +13,26 @@ function calculaFrete($cod_servico, $cep_origem, $cep_destino, $peso, $cod_adm, 
     ############################################
 
     $correios = "http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?nCdEmpresa=".$cod_adm."&sDsSenha=".$senha."&sCepOrigem=".$cep_origem."&sCepDestino=".$cep_destino."&nVlPeso=".$peso."&nCdFormato=1&nVlComprimento=".$comprimento."&nVlAltura=".$altura."&nVlLargura=".$largura."&sCdMaoPropria=n&nVlValorDeclarado=".$valor_declarado."&sCdAvisoRecebimento=n&nCdServico=".$cod_servico."&nVlDiametro=0&StrRetorno=xml";
-    //print $correios;
-    $xml = simplexml_load_file($correios);
+    $xml = @simplexml_load_file($correios, 'SimpleXMLElement');
+    
+    if (!$xml) { 
+        sleep(1);
+        $xml = @simplexml_load_file($correios, 'SimpleXMLElement');
+    
+        if (!$xml) {
+            error_log('Erro na URL: '.$correios);
+            exit();
+        }
+    }
+    
     if($xml->cServico->Erro == '0')
         return $xml->cServico;
     else
         return false;
 }
 
-$xml = calculaFrete($_GET['codigo_servico'],$_GET['cepOrigem'],$_GET['cepDestino'],$_GET['peso'],$_GET['codigo_administrativo'],$_GET['senha']);
+$xml = calculaFrete(41106,'22031-072','22031-072','1.25','','');
+// $xml = calculaFrete($_GET['codigo_servico'],$_GET['cepOrigem'],$_GET['cepDestino'],$_GET['peso'],$_GET['codigo_administrativo'],$_GET['senha']);
 
 $xml->Valor = str_replace(",",".",$xml->Valor);
 
