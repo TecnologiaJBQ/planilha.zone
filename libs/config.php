836423bb6400021bb1fd564de1c95a99f9d51eed
Udlei Nattis <udlei@nati.biz>
2015-10-08 18:20:21 -0300

    Ajuste

--- libs/config.php
+++ libs/config.php
@@ -1,15 +1,15 @@
 <?php
 
 $CORREIOS_CONTRATO = array(
-    ['41106','Correios - PAC - Sem contrato',0],
-    ['40010','Correios - SEDEX - Sem contrato',0],
-    ['40215','Correios - SEDEX 10 - Sem contrato',0],
-    ['40290','Correios - SEDEX Hoje - Sem contrato',0],
-    ['40096','Correios - SEDEX - Código Serviço 40096',1],
-    ['40436','Correios - SEDEX - Código Serviço 40436',1],
-    ['40444','Correios - SEDEX - Código Serviço 40444',1],
-    ['81019','Correios - e-SEDEX - Código Serviço 81019',1],
-    ['41068','Correios - PAC - Código Serviço 41068',1]
+    array('41106','Correios - PAC - Sem contrato',0),
+    array('40010','Correios - SEDEX - Sem contrato',0),
+    array('40215','Correios - SEDEX 10 - Sem contrato',0),
+    array('40290','Correios - SEDEX Hoje - Sem contrato',0),
+    array('40096','Correios - SEDEX - Código Serviço 40096',1),
+    array('40436','Correios - SEDEX - Código Serviço 40436',1),
+    array('40444','Correios - SEDEX - Código Serviço 40444',1),
+    array('81019','Correios - e-SEDEX - Código Serviço 81019',1),
+    array('41068','Correios - PAC - Código Serviço 41068',1)
 );
 
 ?>
